1e9189deb2407e145b9b295e74de1cc5d2d4c71a
ConsuminHelium <32295016+Wizface@users.noreply.github.com>
2020-05-07 21:06:40 +0200

    Add files via upload

--- get_cords.py
+++ get_cords.py
@@ -173,8 +173,32 @@ class Wiiboard:
             logger.info("error occured: " + str(EEFF))
             pass
         print("Center of mass: %s"%str({'x': comx, 'y': comy}))
-    def on_pressed(self):
+    def on_pressed(self, mass):
         logger.info("Button pressed")
+        try: 
+            comx = 1.0
+            comy = 1.0
+            try:
+                total_right  = mass['top_right']   + mass['bottom_right']
+                total_left   = mass['top_left']    + mass['bottom_left']
+                comx = total_right / total_left
+                if comx > 1:
+                    comx = 1 - total_right / total_left
+                else:
+                    comx -= 1
+                total_bottom = mass['bottom_left'] + mass['bottom_right']
+                total_top    = mass['top_left']    + mass['top_right']
+                comy = total_bottom / total_top
+                if comy > 1:
+                    comy = 1 - total_top / total_bottom
+                else:
+                    comy -= 1
+            except Exception as EEFF:
+                logger.info("error occured: " + str(EEFF))
+                pass
+            print("Center of mass: %s"%str({'x': comx, 'y': comy}))
+        except Exception as oof:
+            print("nope " + str(oof))
     def on_released(self):
         logger.info("Button released")
     def close(self):
