399a21c1a80a267ea5ecaa67ca486c4b2aa166f4
ConsuminHelium <32295016+Wizface@users.noreply.github.com>
2020-05-13 19:31:40 +0200

    Add files via upload

--- movement.py
+++ movement.py
@@ -21,6 +21,41 @@ import sys
 
 ####################
 
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
+
+bus = smbus.SMBus(0) # or bus = smbus.SMBus(1) for Revision 2 boards
+address = 0x68       # This is the address value read via the i2cdetect command
+# Now wake the 6050 up as it starts in sleep mode
+bus.write_byte_data(address, power_mgmt_1, 0)
+
+def read_byte(adr):
+    return bus.read_byte_data(address, adr)
+def read_word(adr):
+    high = bus.read_byte_data(address, adr)
+    low = bus.read_byte_data(address, adr+1)
+    val = (high << 8) + low
+    return val
+def read_word_2c(adr):
+    val = read_word(adr)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
+
+
+###########################################################
+###########################################################
+###########################################################
+
 def get_da_daet():
     print("content uwu")
 
@@ -28,32 +63,7 @@ def get_da_daet():
     power_mgmt_1 = 0x6b
     power_mgmt_2 = 0x6c
 
-    def read_byte(adr):
-        return bus.read_byte_data(address, adr)
-
-    def read_word(adr):
-        high = bus.read_byte_data(address, adr)
-        low = bus.read_byte_data(address, adr+1)
-        val = (high << 8) + low
-        return val
-
-    def read_word_2c(adr):
-        val = read_word(adr)
-        if (val >= 0x8000):
-            return -((65535 - val) + 1)
-        else:
-            return val
-
-    def dist(a,b):
-        return math.sqrt((a*a)+(b*b))
-
-    def get_y_rotation(x,y,z):
-        radians = math.atan2(x, dist(y,z))
-        return -math.degrees(radians)
-
-    def get_x_rotation(x,y,z):
-        radians = math.atan2(y, dist(x,z))
-        return math.degrees(radians)
+    
 
     bus = smbus.SMBus(0) # or bus = smbus.SMBus(1) for Revision 2 boards
     address = 0x68       # This is the address value read via the i2cdetect command
